2bf1e91f610fbaba23fdabb5ab3cfc4cb769379f
khuong <volamnhatkhuong@gmail.com>
2024-02-27 23:19:44 +0100

    :sparkles: create chessboard component

---
+++ src/components/Chessboard.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  // @ts-ignore
+  import {Chessboard, FEN} from "cm-chessboard/src/Chessboard";
+
+  import { onMount } from 'svelte';
+
+  let boardElement: HTMLDivElement;
+
+  onMount(async () => {
+    const board = new Chessboard(boardElement, {
+    position: FEN.start,
+    assetsUrl: "../../node_modules/cm-chessboard/assets/"
+  })
+  });
+</script>
+
+<div class="w-96" bind:this={boardElement}></div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import '../app.postcss';
+  import "../../node_modules/cm-chessboard/assets/chessboard.css";
 </script>
 
 <slot />
