f331e5774447dae6464f1ec838c50d59653fa921
khuong <volamnhatkhuong@gmail.com>
2024-03-17 18:39:23 +0100

    :art: improve panel tab switching

--- src/components/Panel.svelte
+++ src/components/Panel.svelte
@@ -1,24 +1,24 @@
 <script lang="ts">
   import { TabGroup, Tab } from '@skeletonlabs/skeleton';
 
-  let currentTab: number = 0;
+  let currentTab: string = "load";
 </script>
 
-<div class="card p-4">
+<div class="card p-4 h-full flex flex-col">
   <header class="card-header">
     <h3 class="h3 text-center">🔎 Analysis</h3>
   </header>
-  <section class="my-4">
+  <section class="my-4 grow">
     <TabGroup justify="justify-center">
-      <Tab bind:group={currentTab} name="tab1" value={0}>(label 1)</Tab>
-      <Tab bind:group={currentTab} name="tab2" value={1}>(label 2)</Tab>
-      <Tab bind:group={currentTab} name="tab3" value={2}>(label 3)</Tab>
+      <Tab bind:group={currentTab} name="load" value="load">💾 Load</Tab>
+      <Tab bind:group={currentTab} name="report" value="report">📊 Report</Tab>
+      <Tab bind:group={currentTab} name="settings" value="settings">🛠️ Settings</Tab>
       <svelte:fragment slot="panel">
-        {#if currentTab === 0}
           (tab panel 1 contents)
-        {:else if currentTab === 1}
+        {#if currentTab === "load"}
+        {:else if currentTab === "report"}
           (tab panel 2 contents)
-        {:else if currentTab === 2}
+        {:else if currentTab === "settings"}
           (tab panel 3 contents)
         {/if}
       </svelte:fragment>
