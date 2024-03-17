5ba5b8bb2fad39795cca25b9a0d0a13d1181d6a1
khuong <volamnhatkhuong@gmail.com>
2024-03-17 16:22:48 +0100

    :sparkles: create EvaluationBar component

---
+++ src/components/EvaluationBar.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  export let score: number = 5;
+
+  const MIN: number = -10;
+  const MAX: number = 10;
+
+  const toPercentage = (score: number): number => ((score - MIN) / (MAX - MIN)) * 100;
+</script>
+
+<div class="relative w-6 bg-black">
+  <div
+    style:height="{toPercentage(score)}%"
+    class="absolute duration-200 bg-white left-0 right-0 bottom-0 h-1/2"
+  ></div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,21 @@
 <script lang="ts">
+  import { RangeSlider } from "@skeletonlabs/skeleton";
   import Chessboard from "../components/Chessboard.svelte";
+  import EvaluationBar from "../components/EvaluationBar.svelte";
   import Panel from "../components/Panel.svelte";
+
+  let value = 0;
 </script>
-<div class="flex flex-wrap justify-center my-6">
-  <div class="size-4/5 md:size-1/2">
-    <Chessboard />
+<div class="flex flex-wrap justify-center my-6 gap-6">
+  <div class="size-4/5 md:size-1/2 flex gap-x-4">
+    <EvaluationBar score={value} />
+    <div class="grow">
+      <Chessboard />
+    </div>
   </div>
-  <div class="w-4/5 md:w-1/4 mt-4 md:ml-6 md:mt-0">
+  <div class="w-4/5 md:w-1/4">
     <Panel />
   </div>
 </div>
+
+<RangeSlider name="range-slider" bind:value={value} min={-10} max={10} step={1} />
