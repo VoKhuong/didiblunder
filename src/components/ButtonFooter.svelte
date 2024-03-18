4bcc29bbb61f721f8e7b66146598a768f2680aca
khuong <volamnhatkhuong@gmail.com>
2024-03-18 19:37:41 +0100

    :sparkles: add onClick prop to ButtonFooter

--- src/components/ButtonFooter.svelte
+++ src/components/ButtonFooter.svelte
@@ -1,1 +1,11 @@
-<button type="button" class="btn btn-lg variant-filled w-full">🚀 Analyze</button>
+<script lang="ts">
+  export let onClick: (event: MouseEvent) => void = () => console.log("onClick event fired");
+</script>
+
+<button
+  type="button"
+  class="btn btn-lg variant-filled w-full"
+  on:click={onClick}
+>
+  🚀 Analyze
+</button>
