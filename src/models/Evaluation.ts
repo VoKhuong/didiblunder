f47f6de844f6f0e829a7c653ecc1d69bbcc18612
khuong <volamnhatkhuong@gmail.com>
2024-03-21 21:18:04 +0100

    :label: create Evaluation type

---
+++ src/models/Evaluation.ts
@@ -0,0 +1,15 @@
+export enum Evaluation {
+  BRILLIANT = 'Brilliant',
+  GREAT = 'Great',
+  BEST = 'Best',
+  EXCELLENT = 'Excellent',
+  GOOD = 'Good',
+  BOOK = 'Book',
+  INACCURACY = 'Inaccuracy',
+  MISTAKE = 'Mistake',
+  MISSED = 'Missed',
+  BLUNDER = 'Blunder',
+  FORCED = 'Forced'
+}
+
+export default Evaluation;
