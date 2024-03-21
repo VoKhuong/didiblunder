820faace5615c011b2ab9a0b5ee00843d76a26ab
khuong <volamnhatkhuong@gmail.com>
2024-03-21 22:25:08 +0100

    :bento: add EvaluationIcon containing evaluation svg

---
+++ src/components/tabs/Feedback.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import Evaluation from '$models/Evaluation';
+  import EvaluationIcon from '../EvaluationIcon.svelte';
+
+  export let san: string;
+  export let evaluation: Evaluation;
+</script>
+
+<div class="flex text-xl items-center">
+  <EvaluationIcon evaluation={evaluation} />
+  {san}
+</div>
--- src/components/tabs/Report.svelte
+++ src/components/tabs/Report.svelte
@@ -4,20 +4,25 @@
   import type { Move } from "chess.js";
   import { getContext } from "svelte";
   import type { Writable } from "svelte/store";
+  import Feedback from "./Feedback.svelte";
+  import Evaluation from "$models/Evaluation";
 
   const history: Writable<Move[]> = getContext('history');
   const move: Writable<number> = getContext('move');
   const position: Writable<string> = getContext('position');
 
   let data: TableSource = toTableSource($history);
+  let san: string = $history[$move]?.san;
 
   const onSelected = (meta: CustomEvent) => {
-    const index = getMoveNumber(meta.detail[0])
-    position.set($history[index].after);
-    move.set(index + 1);
+    move.set(getMoveNumber(meta.detail[0]));
+    position.set($history[$move].after);
   };
+
+  $: san = $history[$move]?.san;
 </script>
 
-<div class="h-28 md:h-32 lg:h-56 overflow-y-auto">
+<div class="h-28 md:h-32 lg:h-56 mb-2 overflow-y-auto">
   <Table source={data} interactive on:selected={onSelected} />
 </div>
+<Feedback san={san} evaluation={Object.values(Evaluation)[$move % Object.keys(Evaluation).length]} />
