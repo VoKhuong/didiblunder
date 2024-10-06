b7a17b1acd7358fa7c64a7ec0e644f289d58393e
khuong <volamnhatkhuong@gmail.com>
2024-10-06 13:59:55 +0200

    :art: move logic to evaluation lib

--- src/components/EvaluationBar.svelte
+++ src/components/EvaluationBar.svelte
@@ -1,29 +1,12 @@
 <script lang="ts">
+  import { toEvaluationHeight } from '$lib/evaluation';
   import type { Evaluation } from '$models/Evaluation';
 
   export let evaluation: Evaluation;
 
-  // score is in centipawns
-  const MIN: number = -1000;
-  const MAX: number = 1000;
-
   let barHeight = 0.5;
 
-  function mapToPercentage(value: number) {
-    const rootValue = Math.sign(value) * Math.pow(Math.abs(value), 0.6);
-    const rootLimit = Math.pow(Math.abs(MAX), 0.6);
-    return Math.min(0.95, Math.max(0.05, (rootValue + rootLimit) / (2 * rootLimit)));
-  }
-
-  $: {
-    barHeight =
-      evaluation.type === 'cp'
-        ? mapToPercentage(evaluation.score)
-        : Math.sign(evaluation.score) > 0
-          ? 1
-          : 0;
-    console.log(barHeight);
-  }
+  $: barHeight = toEvaluationHeight(evaluation);
 </script>
 
 <div class="relative w-6 bg-black">
--- src/lib/evaluation.ts
+++ src/lib/evaluation.ts
@@ -1,13 +1,30 @@
 import type { Evaluation } from "$models/Evaluation";
 
 export function formatScore(evaluation: Evaluation) {
-    if (evaluation.type === "mate") {
-        return `M${Math.abs(evaluation.score)}`;
-    } else {
-        return (evaluation.score / 100).toLocaleString(undefined, {
-            signDisplay: 'exceptZero',
-            minimumFractionDigits: 0,
-            maximumFractionDigits: 2
-        });
-    }
+  if (evaluation.type === "mate") {
+    return `M${Math.abs(evaluation.score)}`;
+  } else {
+    return (evaluation.score / 100).toLocaleString(undefined, {
+      signDisplay: 'exceptZero',
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 2
+    });
+  }
+}
+
+// score is in centipawns
+const LIMIT: number = 1000;
+
+function computeHeightForCP(value: number) {
+  const rootValue = Math.sign(value) * Math.pow(Math.abs(value), 0.6);
+  const rootLimit = Math.pow(LIMIT, 0.6);
+  return Math.min(0.95, Math.max(0.05, (rootValue + rootLimit) / (2 * rootLimit)));
+}
+
+export function toEvaluationHeight(evaluation: Evaluation) {
+  return evaluation.type === 'cp'
+    ? computeHeightForCP(evaluation.score)
+    : Math.sign(evaluation.score) > 0
+      ? 1
+      : 0;
 }
