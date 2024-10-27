afb1376128c02027497e69aaeb83e740d589e361
khuong <volamnhatkhuong@gmail.com>
2024-10-27 19:52:53 +0100

    :sparkles: display opening's name instead of book label

--- src/components/tabs/Feedback.svelte
+++ src/components/tabs/Feedback.svelte
@@ -6,6 +6,7 @@
   export let san: string;
   export let label: Label;
   export let score: string;
+  export let opening: string | undefined;
 </script>
 
 <div class="flex justify-center items-center {getLabelClassColor(label)}">
@@ -13,4 +14,4 @@
   <strong class="h3 font-semibold ml-2 mr-8">{san}</strong>
   <span class="chip variant-filled cursor-default">{score}</span>
 </div>
-<p class="mt-4 text-center text-lg bg-red-400 card variant-ghost p-2">{label}</p>
+<p class="mt-4 text-center text-lg bg-red-400 card variant-ghost p-2">{opening ?? label}</p>
